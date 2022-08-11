3edefc675414d5593ff0d2d4c2de34bf374105b9
InbarShirizly <in.shirizly@gmail.com>
2022-08-11 22:01:43 +0300

    testing the cicd pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -15,7 +15,7 @@ with open("models/wine.pkl", "rb") as file:
 
 class Wine(BaseModel):
     batches: List[conlist(item_type=float, min_items=13, max_items=13)]
-
+#tetse
 
 @app.post("/predict")
 def predict(wine: Wine):
